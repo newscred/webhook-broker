bddae1127312f07f872749eb2ec28a0870758b85
Imran M Yousuf <imyousuf@gmail.com>
2020-11-21 15:44:39 -0500

    Add test for main function
    Signed-off-by: Imran M Yousuf <imyousuf@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"log"
 )
 
 func main() {
-	fmt.Println("Webhook Broker - " + string(GetAppVersion()))
+	log.Println("Webhook Broker - " + string(GetAppVersion()))
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,4 +11,17 @@ import (
 
 func TestGetAppVersion(t *testing.T) {
 	assert.Equal(t, string(GetAppVersion()), "0.1-dev")
+}
+
+func TestMainFunc(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+	main()
+	logString := buf.String()
+	assert.Contains(t, logString, "Webhook Broker")
+	assert.Contains(t, logString, string(GetAppVersion()))
+	t.Log(logString)
 }
