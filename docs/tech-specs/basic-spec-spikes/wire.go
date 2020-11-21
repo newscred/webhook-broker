f9c0ab8f1a879bf48366ba48109190029af6f488
Imran M Yousuf <imyousuf@gmail.com>
2020-11-21 15:32:22 -0500

    Fix Code Climate issues
    Signed-off-by: Imran M Yousuf <imyousuf@gmail.com>

--- docs/tech-specs/basic-spec-spikes/wire.go
+++ docs/tech-specs/basic-spec-spikes/wire.go
@@ -12,11 +12,13 @@ func InitializeDBEngineName() DBEngineName {
 	return "Test"
 }
 
+// InitializeJobQueue creates the Job queue for dispatching to workers
 func InitializeJobQueue() chan Job {
 	wire.Build(NewMaxQueuesConfig, NewJobQueue)
 	return make(chan Job)
 }
 
+// InitializeDispatcher creates Dispatcher to handle messages
 func InitializeDispatcher() *Dispatcher {
 	wire.Build(NewDispatcher, NewMaxWorkersConfig, NewMaxQueuesConfig, NewJobQueue, NewJobPriorityQueue, NewPriorityDispatcherSwitch)
 	return &Dispatcher{}
