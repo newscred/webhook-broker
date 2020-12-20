618c482b32df9125f880379974c70b4745673eb3
Imran M Yousuf <imyousuf@gmail.com>
2020-12-20 17:44:34 -0500

    Enable multistatement flag for MySQL driver
    If this is not enabled than migration will fail. Also add quotation to all
    strings in DDL just for safety.
    
    Signed-off-by: Imran M Yousuf <imyousuf@gmail.com>

--- migration/sqls/000002_create_models_v1.up.sql
+++ migration/sqls/000002_create_models_v1.up.sql
@@ -1,66 +1,66 @@
-CREATE TABLE IF NOT EXISTS producer (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
-    producerId VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `producer` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `producerId` VARCHAR(255) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    createdAt DATETIME NOT NULL,
-    updatedAt DATETIME NOT NULL,
-    UNIQUE (producerId)
+    `token` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
+    UNIQUE (`producerId`)
 );
 
-CREATE TABLE IF NOT EXISTS channel (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
-    channelId VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `channel` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `channelId` VARCHAR(255) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    createdAt DATETIME NOT NULL,
-    updatedAt DATETIME NOT NULL,
+    `token` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
     UNIQUE (channelId)
 );
 
-CREATE TABLE IF NOT EXISTS consumer (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
-    consumerId VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `consumer` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `consumerId` VARCHAR(255) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    callbackUrl TEXT(1000) NOT NULL,
-    channelId VARCHAR(255) NOT NULL,
-    createdAt DATETIME NOT NULL,
-    updatedAt DATETIME NOT NULL,
-    UNIQUE (consumerId, channelId),
-    CONSTRAINT channelConsumerRef FOREIGN KEY (channelId) REFERENCES channel(channelId) ON UPDATE CASCADE ON DELETE RESTRICT
+    `token` VARCHAR(255) NOT NULL,
+    `callbackUrl` TEXT(1000) NOT NULL,
+    `channelId` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
+    UNIQUE (`consumerId`, `channelId`),
+    CONSTRAINT `channelConsumerRef` FOREIGN KEY (`channelId`) REFERENCES channel(`channelId`) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS message (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
-    messageId VARCHAR(255) NOT NULL,
-    payload MEDIUMTEXT NOT NULL,
-    contentType VARCHAR(255) NOT NULL,
-    priority INTEGER NOT NULL,
-    status INTEGER NOT NULL,
-    channelId VARCHAR(255) NOT NULL,
-    producerId VARCHAR(255) NOT NULL,
-    receivedAt DATETIME NOT NULL,
-    outboxedAt DATETIME NOT NULL,
-    createdAt DATETIME NOT NULL,
-    updatedAt DATETIME NOT NULL,
-    UNIQUE (messageId, channelId),
-    CONSTRAINT channelMessageRef FOREIGN KEY (channelId) REFERENCES channel(channelId) ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT producerMessageRef FOREIGN KEY (producerId) REFERENCES producer(producerId) ON UPDATE CASCADE ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS `message` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `messageId` VARCHAR(255) NOT NULL,
+    `payload` MEDIUMTEXT NOT NULL,
+    `contentType` VARCHAR(255) NOT NULL,
+    `priority` INTEGER NOT NULL,
+    `status` INTEGER NOT NULL,
+    `channelId` VARCHAR(255) NOT NULL,
+    `producerId` VARCHAR(255) NOT NULL,
+    `receivedAt` DATETIME NOT NULL,
+    `outboxedAt` DATETIME NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
+    UNIQUE (`messageId`, `channelId`),
+    CONSTRAINT `channelMessageRef` FOREIGN KEY (`channelId`) REFERENCES channel(`channelId`) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT `producerMessageRef` FOREIGN KEY (`producerId`) REFERENCES producer(`producerId`) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS job (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
-    messageId VARCHAR(255) NOT NULL,
-    consumerId VARCHAR(255) NOT NULL,
-    status INTEGER NOT NULL,
-    retryAttemptCount INTEGER NOT NULL DEFAULT 0,
-    statusChangedAt DATETIME NOT NULL,
-    dispatchReceivedAt DATETIME NOT NULL,
-    earliestNextAttemptAt DATETIME NOT NULL,
-    createdAt DATETIME NOT NULL,
-    updatedAt DATETIME NOT NULL,
-    UNIQUE (messageId, consumerId),
-    CONSTRAINT messageJobRef FOREIGN KEY (messageId) REFERENCES message(id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT consumerJobRef FOREIGN KEY (consumerId) REFERENCES consumer(id) ON UPDATE CASCADE ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS `job` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `messageId` VARCHAR(255) NOT NULL,
+    `consumerId` VARCHAR(255) NOT NULL,
+    `status` INTEGER NOT NULL,
+    `retryAttemptCount` INTEGER NOT NULL DEFAULT 0,
+    `statusChangedAt` DATETIME NOT NULL,
+    `dispatchReceivedAt` DATETIME NOT NULL,
+    `earliestNextAttemptAt` DATETIME NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
+    UNIQUE (`messageId`, `consumerId`),
+    CONSTRAINT `messageJobRef` FOREIGN KEY (`messageId`) REFERENCES message(`id`) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT `consumerJobRef` FOREIGN KEY (`consumerId`) REFERENCES consumer(`id`) ON UPDATE CASCADE ON DELETE RESTRICT
 );
