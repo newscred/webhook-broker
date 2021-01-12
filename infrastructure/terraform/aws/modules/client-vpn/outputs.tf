f8efba35df097d6f8d18214fd813c84058368594
Imran M Yousuf <imyousuf@gmail.com>
2021-01-12 09:39:40 -0500

    Move terraform code to its own repo so that it can be published
    The naming convention and project directory dictates that root folder has
    the .tf files with specialized reusable modules in `/modules` hence since
    we intend to publish webhook broker modules to terraform registry we are
    moving it to its own github -
    https://github.com/imyousuf/terraform-aws-webhook-broker
    
    Signed-off-by: Imran M Yousuf <imyousuf@gmail.com>

--- infrastructure/terraform/aws/modules/client-vpn/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpn_arn" {
-  description = "Client VPN ARN"
-  value       = aws_ec2_client_vpn_endpoint.vpn.arn
-}
-
-output "vpn_id" {
-  description = "Client VPN ID"
-  value       = aws_ec2_client_vpn_endpoint.vpn.id
-}
