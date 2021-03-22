7bbbb39c6d51e1638fea10e5dd78b21f4a3431b8
Imran M Yousuf <imyousuf@gmail.com>
2021-03-22 12:53:29 -0400

    Prepare 0.1.0-beta-3 release
    Signed-off-by: Imran M Yousuf <imyousuf@gmail.com>

--- main_test.go
+++ main_test.go
@@ -38,7 +38,7 @@ const (
 )
 
 func TestGetAppVersion(t *testing.T) {
-	assert.Equal(t, string(GetAppVersion()), "0.1.0-dev")
+	assert.Equal(t, string(GetAppVersion()), "0.1.0-beta-3")
 }
 
 var mainFunctionBreaker = func(stop *chan os.Signal) {
