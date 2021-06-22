2f0a2628b66f0a416d02b7208a45992c2a6b7974
Imran M Yousuf <imyousuf@gmail.com>
2021-06-22 17:43:04 -0400

    Prepare maintenance release
    Signed-off-by: Imran M Yousuf <imyousuf@gmail.com>

--- main_test.go
+++ main_test.go
@@ -38,7 +38,7 @@ const (
 )
 
 func TestGetAppVersion(t *testing.T) {
-	assert.Equal(t, string(GetAppVersion()), "0.1.0")
+	assert.Equal(t, string(GetAppVersion()), "0.1.1")
 }
 
 var mainFunctionBreaker = func(stop *chan os.Signal) {
