a542dfff7f0273336f540e78e38821a3de4662dd
Bishwajit <bishwajit0020@gmail.com>
2023-02-02 11:04:38 +0600

    add test for priority queue

---
+++ dispatcher/queue_test.go
@@ -0,0 +1,56 @@
+package dispatcher
+
+import (
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/newscred/webhook-broker/storage/data"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPriorityQueueBasic(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+	pq := NewJobPriorityQueue()
+	priorities := rand.Perm(100)
+
+	for _, priority := range priorities {
+		messagePayload := strconv.Itoa(priority)
+		msg, _ := data.NewMessage(channel, producer, messagePayload, "application/json")
+		dj := &data.DeliveryJob{Message: msg}
+		pq.Enqueue(&Job{dj, uint(priority)})
+	}
+	for expectedPriority := 99; expectedPriority >= 0; expectedPriority-- {
+		assert.Equal(t, pq.Len(), expectedPriority+1)
+		topJob := pq.Dequeue()
+		assert.Equal(t, uint(expectedPriority), topJob.Priority)
+		assert.Equal(t, strconv.Itoa(expectedPriority), topJob.Data.Message.Payload)
+	}
+}
+
+func BenchmarkPriorityQueue(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		stressPriorityQueue()
+	}
+}
+
+func stressPriorityQueue() {
+	MaxQueueSize := 100000
+	contentType := "application/json"
+
+	pq := NewJobPriorityQueue()
+	for i := 0; i < MaxQueueSize; i++ {
+		priority := i % 3
+		messagePayload := strconv.Itoa(priority)
+		msg, _ := data.NewMessage(channel, producer, messagePayload, contentType)
+		dj := &data.DeliveryJob{Message: msg}
+		pq.Enqueue(&Job{dj, uint(priority)})
+	}
+
+	for i := 0; i < MaxQueueSize; i++ {
+		_ = pq.Dequeue()
+	}
+
+}
