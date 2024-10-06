3f94204728535deaf5bf57a5e9fdcf86a54aceeb
Imran M Yousuf <imran.yousuf@optimizely.com>
2024-10-06 15:05:53 -0400

    Avoid sqlite driver related code from db normalizer
    This is done to ensure the project builds (not run tests) in Windows

--- storage/dberror_normalizer_win.go
+++ storage/dberror_normalizer_win.go
@@ -1,27 +1,15 @@
-//go:build linux || darwin
+//go:build windows
 
 package storage
 
 import (
 	"github.com/go-sql-driver/mysql"
-	sqlite "github.com/mattn/go-sqlite3"
 )
 
 func normalizeDBError(mysqlDriverErr error, mappedError map[uint16]error) (err error) {
 	err = mysqlDriverErr
 	if mysqlErr, ok := err.(*mysql.MySQLError); ok {
 		err = lookup(mysqlErr.Number, mappedError, mysqlErr)
-	} else if sqliteErr, ok := err.(*sqlite.ErrNoExtended); ok {
-		switch *sqliteErr {
-		case sqlite.ErrConstraintUnique:
-			err = lookup(1062, mappedError, sqliteErr)
-		}
-
-	} else if sqliteErr, ok := err.(*sqlite.ErrNo); ok {
-		switch *sqliteErr {
-		case sqlite.ErrConstraint:
-			err = lookup(1062, mappedError, sqliteErr)
-		}
 	}
 	return err
 }
