2eb87519055f9d359f49295960ae5a75c1f90d01
ashiqursuperfly <ashiqur.buet16@gmail.com>
2025-07-23 07:06:02 +0000

    set version to 0.3.0-dev to lock 0.2.3 release

--- main_test.go
+++ main_test.go
@@ -38,7 +38,7 @@ const (
 )
 
 func TestGetAppVersion(t *testing.T) {
-	assert.Equal(t, string(GetAppVersion()), "0.2.3")
+	assert.Equal(t, string(GetAppVersion()), "0.3.0-dev")
 }
 
 var waitForPort = func(portNum int) {
