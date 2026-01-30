2a1f351612fd3de93d72a880c3df3c3677328758
Shadman Shadab <oneshadab@gmail.com>
2026-01-30 18:18:57 +0000

    fix: Remove conditional creation of indexes

--- migration/sqls/000011_add_jobs_query_indexes.down.sql
+++ migration/sqls/000011_add_jobs_query_indexes.down.sql
@@ -1,8 +1,8 @@
 -- Restore original job indexes, drop the replacement
-DROP INDEX IF EXISTS `job_status_created_id` ON `job`;
-CREATE INDEX IF NOT EXISTS `force_timeout_job` ON `job` (`status`, `statusChangedAt`, `id`, `createdAt`);
-CREATE INDEX IF NOT EXISTS `order_job_createdAt_id` ON `job` (`createdAt`, `id`);
+DROP INDEX `job_status_created_id` ON `job`;
+CREATE INDEX `force_timeout_job` ON `job` (`status`, `statusChangedAt`, `id`, `createdAt`);
+CREATE INDEX `order_job_createdAt_id` ON `job` (`createdAt`, `id`);
 
 -- Restore original message index, drop the replacement
-DROP INDEX IF EXISTS `message_status_created_id` ON `message`;
-CREATE INDEX IF NOT EXISTS `order_msg_createdAt_id` ON `message` (`createdAt`, `id`);
+DROP INDEX `message_status_created_id` ON `message`;
+CREATE INDEX `order_msg_createdAt_id` ON `message` (`createdAt`, `id`);
